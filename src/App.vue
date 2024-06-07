2c54fc0db108517681238abbe0fd026c237be04f
willl <willl@zju.edu.cn>
2024-06-07 15:11:24 +0800

    update: input Cesium

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <cesium-viewer></cesium-viewer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import CesiumViewer from './components/CesiumViewer.vue'; 
+</script>
+
+<style scoped> 
+  #cesiumContainer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 </style>
---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+import * as Cesium from 'cesium'
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer')
+})
+</script>
+
