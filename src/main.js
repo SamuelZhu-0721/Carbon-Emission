827de02af4c7399d63ca0e957a148cb615020f76
willl <willl@zju.edu.cn>
2024-06-12 21:51:38 +0800

    merge

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
-import ECharts from 'vue-echarts'
-import 'echarts'
-
-createApp(App).use(Antd).mount("#app");
